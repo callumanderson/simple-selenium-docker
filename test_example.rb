f6f518d7b52f4df1185ec8d84a385d47286c3f06
cba <callum.anderson@mendeley.com>
2015-06-04 20:25:09 +0100

    Reverting changes

--- test_example.rb
+++ test_example.rb
@@ -1,27 +1,23 @@
 require 'watir-webdriver'
 
+
 browsers = [:chrome, :firefox]
 
+browsers.each do |b|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.new
+  ip = URI.parse(`boot2docker ip`)
 
-def get_browser(capabilities, name, ip)
-  capabilities.browser_name = name
+
+  capabilities.browser_name = b
+
   browser = Watir::Browser.new(
       :remote,
       :url => "http://#{ip}:4444/wd/hub",
       :desired_capabilities => capabilities
   )
-  browser
-end
-
-browsers.each do |b|
-  ip = URI.parse(`boot2docker ip`)
-  capabilities = Selenium::WebDriver::Remote::Capabilities.new
-  browser = get_browser(capabilities, b, ip)
-
 
   puts "starting browser as #{browser}"
   browser.goto 'http://whatismybrowser.com'
   puts browser.div(:class => 'string-major').text
   browser.close
-
 end
