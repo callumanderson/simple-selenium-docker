2ca028ca758298eadd3fccbb59901879821a34cb
cba <callum.anderson@mendeley.com>
2015-06-04 09:52:24 +0100

    working standalone now

--- test_example.rb
+++ test_example.rb
@@ -4,7 +4,18 @@ require 'watir-webdriver'
 browsers = [:chrome, :firefox]
 
 browsers.each do |b|
-  browser = Watir::Browser.new(browser = b)
+  capabilities = Selenium::WebDriver::Remote::Capabilities.new
+  ip = URI.parse(`boot2docker ip`)
+
+
+  capabilities.browser_name = b
+
+  browser = Watir::Browser.new(
+      :remote,
+      :url => "http://#{ip}:4444/wd/hub",
+      :desired_capabilities => capabilities
+  )
+
   puts "starting browser as #{browser}"
   browser.goto 'http://whatismybrowser.com'
   puts browser.div(:class => 'string-major').text
